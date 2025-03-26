5ddbf816b576e9e3303dfc9e07b2715fc6b30e1c
Christian Salvador <41243724+KittenInAMitten@users.noreply.github.com>
2025-03-26 11:42:49 -0600

    Separated Functions and Added Networking

--- scripts/app.py
+++ scripts/app.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template, jsonify, request
-from picrawler import Picrawler
+# from picrawler import Picrawler
+import time
 from time import sleep
 import readchar
 import threading
+import socket
 
 
 # Variables
-crawler = Picrawler() 
+# crawler = Picrawler() 
 speed = 60
 tilt_value = 0
 in_action = False
+waiting = False
+start_time = 0
+last_time = 0
 
 # Debug variables
 KEYBOARD_MODE = True
@@ -27,130 +32,131 @@ Press keys on keyboard to control PiCrawler!
     Ctrl^C: Quit
 '''
 
-def show_info():
-    # print("\033[H\033[J",end='')  # clear terminal windows 
-    print(manual)
+'''
+# def show_info():
+#     # print("\033[H\033[J",end='')  # clear terminal windows 
+#     print(manual)
 
 
-def move_forward():
-    global tilt_value
-    global in_action
-    if in_action:
-        return
-    in_action = True
-    tilt_value = 0
-    crawler.do_action('forward',1,speed)
-    crawler.do_action('stand', speed)
-    print("Moved forward")
-    show_info()
-    in_action = False
-    return
+# def move_forward():
+#     global tilt_value
+#     global in_action
+#     if in_action:
+#         return
+#     in_action = True
+#     tilt_value = 0
+#     crawler.do_action('forward',1,speed)
+#     crawler.do_action('stand', speed)
+#     print("Moved forward")
+#     show_info()
+#     in_action = False
+#     return
     
-def move_backward():
-    global tilt_value
-    global in_action
-    if in_action:
-        return
-    in_action = True
-    tilt_value = 0
-    crawler.do_action('backward',1,speed)
-    crawler.do_action('stand', speed)
-    print("Moved backward")
-    show_info()
-    in_action = False
-    return
+# def move_backward():
+#     global tilt_value
+#     global in_action
+#     if in_action:
+#         return
+#     in_action = True
+#     tilt_value = 0
+#     crawler.do_action('backward',1,speed)
+#     crawler.do_action('stand', speed)
+#     print("Moved backward")
+#     show_info()
+#     in_action = False
+#     return
     
-def turn_left():
-    global tilt_value
-    global in_action
-    if in_action:
-        return
-    in_action = True
-    tilt_value = 0
-    crawler.do_action('turn left',1,speed)
-    crawler.do_action('stand', speed)
-    print("Turned left")
-    show_info()
-    in_action = False
-    return
+# def turn_left():
+#     global tilt_value
+#     global in_action
+#     if in_action:
+#         return
+#     in_action = True
+#     tilt_value = 0
+#     crawler.do_action('turn left',1,speed)
+#     crawler.do_action('stand', speed)
+#     print("Turned left")
+#     show_info()
+#     in_action = False
+#     return
     
-def turn_right():
-    global tilt_value
-    global in_action
-    if in_action:
-        return
+# def turn_right():
+#     global tilt_value
+#     global in_action
+#     if in_action:
+#         return
     
-    in_action = True
-    tilt_value = 0
-    crawler.do_action('turn right',1,speed)
-    crawler.do_action('stand', speed)
-    print("Turned right")
-    show_info()
-    in_action = False
-    return
+#     in_action = True
+#     tilt_value = 0
+#     crawler.do_action('turn right',1,speed)
+#     crawler.do_action('stand', speed)
+#     print("Turned right")
+#     show_info()
+#     in_action = False
+#     return
 
-def tilt_up():
-    global tilt_value
-    global in_action
-    if in_action:
-        return
+# def tilt_up():
+#     global tilt_value
+#     global in_action
+#     if in_action:
+#         return
     
-    in_action = True
-    if tilt_value < 25:
-        tilt_value += 5
+#     in_action = True
+#     if tilt_value < 25:
+#         tilt_value += 5
         
-    coords = [
-        # stand
-        [[45, 45, (-50 - tilt_value)], [45, 45, (-50 - tilt_value)], [45, 45, (-50 + tilt_value)], [45, 45, (-50 + tilt_value)]]
-    ]
-    for coord in coords:
-        crawler.do_step(coord, 60)
-    show_info()
-    in_action = False
-    return
-    # crawler.do_action('look up',1,speed)
-    # print("Tilted up")
-    # show_info()
-    # return
+#     coords = [
+#         # stand
+#         [[45, 45, (-50 - tilt_value)], [45, 45, (-50 - tilt_value)], [45, 45, (-50 + tilt_value)], [45, 45, (-50 + tilt_value)]]
+#     ]
+#     for coord in coords:
+#         crawler.do_step(coord, 60)
+#     show_info()
+#     in_action = False
+#     return
+#     # crawler.do_action('look up',1,speed)
+#     # print("Tilted up")
+#     # show_info()
+#     # return
 
-def tilt_down():
-    global tilt_value
-    global in_action
-    if in_action:
-        return
-    in_action = True
+# def tilt_down():
+#     global tilt_value
+#     global in_action
+#     if in_action:
+#         return
+#     in_action = True
     
-    if tilt_value > -25:
-        tilt_value -= 5
-    coords = [
-        # stand
-        [[45, 45, (-50 - tilt_value)], [45, 45, (-50 - tilt_value)], [45, 45, (-50 + tilt_value)], [45, 45, (-50 + tilt_value)]]
-    ]
-    for coord in coords:
-        crawler.do_step(coord, 60)
-    in_action = False
-    return
+#     if tilt_value > -25:
+#         tilt_value -= 5
+#     coords = [
+#         # stand
+#         [[45, 45, (-50 - tilt_value)], [45, 45, (-50 - tilt_value)], [45, 45, (-50 + tilt_value)], [45, 45, (-50 + tilt_value)]]
+#     ]
+#     for coord in coords:
+#         crawler.do_step(coord, 60)
+#     in_action = False
+#     return
 
-    # crawler.do_action('look down',1,speed)
-    # print("Tilted down")
-    # return
+#     # crawler.do_action('look down',1,speed)
+#     # print("Tilted down")
+#     # return
  
-def stand():
-    global tilt_value
-    global in_action
-    if in_action:
-        return
-    in_action = True
-    crawler.do_action("stand", speed=60)
-    # coords = [
-    #     # stand
-    #     [[45, 45, -50], [45, 45, -50], [45, 45, -50], [45, 45, -50]],
-    #     [[45, 45, -75], [45, 45, -75], [45, 45, -35], [45, 45, -35]],
-    # ]
-    # for coord in coords:
-    #     crawler.do_step(coord, 60)
-    in_action = False
-    return
+# def stand():
+#     global tilt_value
+#     global in_action
+#     if in_action:
+#         return
+#     in_action = True
+#     crawler.do_action("stand", speed=60)
+#     # coords = [
+#     #     # stand
+#     #     [[45, 45, -50], [45, 45, -50], [45, 45, -50], [45, 45, -50]],
+#     #     [[45, 45, -75], [45, 45, -75], [45, 45, -35], [45, 45, -35]],
+#     # ]
+#     # for coord in coords:
+#     #     crawler.do_step(coord, 60)
+#     in_action = False
+#     return
 
 def lock_pos():
     # global in_action
@@ -197,7 +203,12 @@ def main_loop():
             
             sleep(0.02)          
 
+'''
+
 app = Flask(__name__)
+
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientsocket.connect(('localhost', 8089))
 
 @app.route('/')
 def index():
@@ -205,25 +216,50 @@ def index():
 
 @app.route('/command', methods=['POST'])
 def command():
+    global waiting
+    global start_time
+    global last_time
     data = request.json
     # show_info()
     # if data['action'] != "lock": 
-    print(f"Received command: {data['action']}")
-    if data['action'] == "forward":
-        move_forward()
-    elif data['action'] == "backward":
-        move_backward()
-    elif data['action'] == "left":
-        turn_left()
-    elif data['action'] == "right":
-        turn_right()
-    elif data['action'] == "tilt_up":
-        tilt_up()
-    elif data['action'] == "tilt_down":
-        tilt_down()
-    # elif data['action'] == "lock":
-    #     lock_pos()
-    # show_info()  
+    if waiting == False:
+        start_time = time.time()
+        print(f"Received command: {data['action']}")
+        waiting = True
+        if data['action'] == "forward":
+            clientsocket.send(bytes(data['action'], 'UTF-8'))
+            # move_forward()
+        elif data['action'] == "backward":
+            clientsocket.send(bytes(data['action'], 'UTF-8'))
+            # move_backward()
+        elif data['action'] == "left":
+            clientsocket.send(bytes(data['action'], 'UTF-8'))
+            # turn_left()
+        elif data['action'] == "right":
+            clientsocket.send(bytes(data['action'], 'UTF-8'))
+            # turn_right()
+        elif data['action'] == "tilt_up":
+            clientsocket.send(bytes(data['action'], 'UTF-8'))
+            # tilt_up()
+        elif data['action'] == "tilt_down":
+            clientsocket.send(bytes(data['action'], 'UTF-8'))
+            # tilt_down()
+        # elif data['action'] == "lock":
+        #     lock_pos()
+        # show_info()  
+        sleep(0.5)
+        while waiting:
+            buf = clientsocket.recv(64)
+            reply = buf.decode('utf-8')
+            if len(buf) > 0:
+                if reply == 'ok':
+                    waiting = False
+                    break
+    else:
+        last_time = time.time()
+        if last_time - start_time > 5.0:
+            waiting = False
+    
     return jsonify({"status": "success", "action": data['action']})
 
 @app.route('/video_feed')
@@ -241,7 +277,8 @@ if __name__ == '__main__':
     # Vilib.color_detect("red") 
     # print("Camera started")
     
-    crawler.do_action("stand", speed=60)
+    
+    # crawler.do_action("stand", speed=60)
     app.run(debug=True, host='0.0.0.0')
     
     
--- scripts/main.py
+++ scripts/main.py
@@ -1,180 +1,293 @@
-# from vilib import Vilib
+from vilib import Vilib
 from picrawler import Picrawler
 from time import sleep
 import readchar
+import socket
 
 # Variables
 crawler = Picrawler() 
-speed = 70
+speed = 60
+tilt_value = 0
+in_action = False
+
+ALERT_MODE = False
+MANUAL_MODE = True
 
 # Debug variables
 KEYBOARD_MODE = False
-	
+    
 
 manual = '''
 Press keys on keyboard to control PiCrawler!
-	W: Forward
-	A: Turn left
-	S: Backward
-	D: Turn right
-	R: Tilt up
-	F: Tilt down
+    W: Forward
+    A: Turn left
+    S: Backward
+    D: Turn right
+    R: Tilt up
+    F: Tilt down
 
-	Ctrl^C: Quit
+    Ctrl^C: Quit
 '''
 
 def show_info():
-	# print("\033[H\033[J",end='')  # clear terminal windows 
-	# print(manual)
-	return
+    # print("\033[H\033[J",end='')  # clear terminal windows 
+    # print(manual)
+    return
 
 
 def move_forward():
-	crawler.do_action('forward',1,speed)
-	show_info()
-	return
-	
+    global tilt_value
+    global in_action
+    if in_action:
+        return
+    in_action = True
+    tilt_value = 0
+    crawler.do_action('forward',1,speed)
+    # crawler.do_action('stand', speed)
+    print("Moved forward")
+    show_info()
+    in_action = False
+    return
+    
 def move_backward():
-	crawler.do_action('backward',1,speed) 
-	show_info()
-	return
-	
+    global tilt_value
+    global in_action
+    if in_action:
+        return
+    in_action = True
+    tilt_value = 0
+    crawler.do_action('backward',1,speed)
+    # crawler.do_action('stand', speed)
+    print("Moved backward")
+    show_info()
+    in_action = False
+    return
+    
 def turn_left():
-	crawler.do_action('turn left',1,speed)
-	show_info()
-	return
-	
+    global tilt_value
+    global in_action
+    if in_action:
+        return
+    in_action = True
+    tilt_value = 0
+    crawler.do_action('turn left',1,speed)
+    # crawler.do_action('stand', speed)
+    print("Turned left")
+    show_info()
+    in_action = False
+    return
+    
 def turn_right():
-	crawler.do_action('turn right',1,speed)
-	show_info()
-	return
+    global tilt_value
+    global in_action
+    if in_action:
+        return
+    
+    in_action = True
+    tilt_value = 0
+    crawler.do_action('turn right',1,speed)
+    # crawler.do_action('stand', speed)
+    print("Turned right")
+    show_info()
+    in_action = False
+    return
 
 def tilt_up():
-	# crawler.do_action('look_up', speed)
-	coords = [
-		# stand
-		[[45, 45, -50], [45, 45, -50], [45, 45, -50], [45, 45, -50]],
-		[[45, 45, -76], [45, 45, -76], [45, 45, -38], [45, 45, -30]],
-	]
-
-	for coord in coords:
-		crawler.do_step(coord, 60)
-	show_info()
-	return
+    global tilt_value
+    global in_action
+    if in_action:
+        return
+    
+    in_action = True
+    if tilt_value < 25:
+        tilt_value += 5
+        
+    coords = [
+        # stand
+        [[45, 45, (-50 - tilt_value)], [45, 45, (-50 - tilt_value)], [45, 45, (-50 + tilt_value)], [45, 45, (-50 + tilt_value)]]
+    ]
+    for coord in coords:
+        crawler.do_step(coord, 60)
+    show_info()
+    in_action = False
+    return
+    # crawler.do_action('look up',1,speed)
+    # print("Tilted up")
+    # show_info()
+    # return
 
 def tilt_down():
-	# crawler.do_action('look_down', speed)
-	coords = [
-		# stand
-		[[45, 45, -50], [45, 45, -50], [45, 45, -50], [45, 45, -50]],
-		[[45, 45, -28], [45, 45, -40], [45, 45, -68], [45, 45, -76]],
-	]
-	for coord in coords:
-		crawler.do_step(coord, 60)
-	return
+    global tilt_value
+    global in_action
+    if in_action:
+        return
+    in_action = True
+    
+    if tilt_value > -25:
+        tilt_value -= 5
+    coords = [
+        # stand
+        [[45, 45, (-50 - tilt_value)], [45, 45, (-50 - tilt_value)], [45, 45, (-50 + tilt_value)], [45, 45, (-50 + tilt_value)]]
+    ]
+    for coord in coords:
+        crawler.do_step(coord, 60)
+    in_action = False
+    return
 
+    # crawler.do_action('look down',1,speed)
+    # print("Tilted down")
+    # return
+ 
 def stand():
-	coords = [
-		# stand
-		[[45, 45, -50], [45, 45, -50], [45, 45, -50], [45, 45, -50]],
-		[[45, 45, -75], [45, 45, -75], [45, 45, -35], [45, 45, -35]],
-	]
-	for coord in coords:
-		crawler.do_step(coord, 60)
-	return
-	
+    global tilt_value
+    global in_action
+    if in_action:
+        return
+    in_action = True
+    crawler.do_action("stand", speed=60)
+    # coords = [
+    #     # stand
+    #     [[45, 45, -50], [45, 45, -50], [45, 45, -50], [45, 45, -50]],
+    #     [[45, 45, -75], [45, 45, -75], [45, 45, -35], [45, 45, -35]],
+    # ]
+    # for coord in coords:
+    #     crawler.do_step(coord, 60)
+    in_action = False
+    return
+    
 
 def main(): 
 
-	# Vilib.camera_start()
-	# Vilib.display()
-	# Vilib.color_detect("red") 
+    Vilib.camera_start()
+    Vilib.display(local=False,web=True)
+    Vilib.color_detect("red") 
+    
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind(('localhost', 8089))
+    serversocket.listen(5) # become a server socket, maximum 5 connections
 
-	try:
-		show_info()	
-		if(KEYBOARD_MODE):
-			while True:
-				key = readchar.readkey()
-				key = key.lower()
-				if key in('wsadrfg'):
-					if 'w' == key:
-						move_forward()	
-					elif 's' == key:
-						move_backward()		 
-					elif 'a' == key:
-						turn_left()		  
-					elif 'd' == key:
-						turn_right()
-					elif 'r' == key:
-						print("tilting up")
-						tilt_up()
-					elif 'f' == key:
-						print("tilting dow")
-						tilt_down()
-					elif 'g' == key:
-						print("standing")
-						stand()
-					sleep(0.05)
-					show_info()  
+    print('Mother Flocker activated. Awaiting commands.')
+    
+    try:
+        connection, address = serversocket.accept()
+        while True:
+            buf = connection.recv(64)
+            command = buf.decode('utf-8')
+            if len(buf) > 0:
+                print(command)
+                if MANUAL_MODE:
+                    if command == "forward":
+                        move_forward()	
+                    elif command == "backward":
+                        move_backward()		 
+                    elif command == "left":
+                        turn_left()		  
+                    elif command == "right":
+                        turn_right()
+                    elif command == "tilt_up":
+                        print("tilting up")
+                        tilt_up()
+                    elif command == "tilt_down":
+                        print("tilting dow")
+                        tilt_down()
+                    elif command == "stand":
+                        print("standing")
+                        stand()
+                    connection.send(bytes('ok', 'UTF-8'))
+                    sleep(0.05)
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        print(f'Error:\n {e}')
+    finally:
+        crawler.do_action("stand", speed=60)
+        sleep(.1)		
+    '''    
+    # try:
+    #     show_info()	
+    #     if(KEYBOARD_MODE):
+    #         while True:
+    #             key = readchar.readkey()
+    #             key = key.lower()
+    #             if key in('wsadrfg'):
+    #                 if 'w' == key:
+    #                     move_forward()	
+    #                 elif 's' == key:
+    #                     move_backward()		 
+    #                 elif 'a' == key:
+    #                     turn_left()		  
+    #                 elif 'd' == key:
+    #                     turn_right()
+    #                 elif 'r' == key:
+    #                     print("tilting up")
+    #                     tilt_up()
+    #                 elif 'f' == key:
+    #                     print("tilting dow")
+    #                     tilt_down()
+    #                 elif 'g' == key:
+    #                     print("standing")
+    #                     stand()
+    #                 sleep(0.05)
+    #                 show_info()  
 
-				elif key == readchar.key.CTRL_C:
-					print("\n Quit") 
-					break	
-				
-				sleep(0.02)  
-		else:
-			coords = crawler.move_list['stand']
-			# print("Input [0-4] to select leg. Current coords:")
-			last_key = None
-			while True:
-				print("Input [0-4] to select leg.")
-				print(coords)
-				key = input()
+    #             elif key == readchar.key.CTRL_C:
+    #                 print("\n Quit") 
+    #                 break	
+                
+    #             sleep(0.02)  
+    #     else:
+    #         coords = crawler.move_list['stand']
+    #         # print("Input [0-4] to select leg. Current coords:")
+    #         last_key = None
+    #         while True:
+    #             print("Input [0-4] to select leg.")
+    #             print(coords)
+    #             key = input()
 
-				if key == '':
-					# print(actions[last_key])
-					# actions_dict[actions[last_key]](my_spider)
-					
-					print("Input [0-4] to select leg.")
-				else:
-					# key = String(key)
-					if key not in ["0", "1", "2", "3"]:
-						print("Invalid leg")
-					else:
-						last_key = int(key)
-						print("Input [X Y Z] to change orientation.")
-						s = input()
-						nums = list(map(int, s.split()))
-						coords = crawler.move_list['stand']
-						coords[0][last_key] = nums
-						print(coords)
-						for coord in coords:
-							crawler.do_step(coord, 60)
+    #             if key == '':
+    #                 # print(actions[last_key])
+    #                 # actions_dict[actions[last_key]](my_spider)
+                    
+    #                 print("Input [0-4] to select leg.")
+    #             else:
+    #                 # key = String(key)
+    #                 if key not in ["0", "1", "2", "3"]:
+    #                     print("Invalid leg")
+    #                 else:
+    #                     last_key = int(key)
+    #                     print("Input [X Y Z] to change orientation.")
+    #                     s = input()
+    #                     nums = list(map(int, s.split()))
+    #                     coords = crawler.move_list['stand']
+    #                     coords[0][last_key] = nums
+    #                     print(coords)
+    #                     for coord in coords:
+    #                         crawler.do_step(coord, 60)
 
-				# sleep(2)
-				# wave_hand(my_spider)
-				# shake_hand(my_spider)
-				# fighting(my_spider)
-				# excited(my_spider)
-				# play_dead(my_spider)
-				# relax_legs(my_spider)
-				# nod(my_spider)
-				# shake_head(my_spider)
-				# look_left(my_spider)
-				# look_right(my_spider)
-				# look_up(my_spider)
-				# look_down(my_spider)
-				# warm_up(my_spider)
-				# push_up(my_spider)
+    #             # sleep(2)
+    #             # wave_hand(my_spider)
+    #             # shake_hand(my_spider)
+    #             # fighting(my_spider)
+    #             # excited(my_spider)
+    #             # play_dead(my_spider)
+    #             # relax_legs(my_spider)
+    #             # nod(my_spider)
+    #             # shake_head(my_spider)
+    #             # look_left(my_spider)
+    #             # look_right(my_spider)
+    #             # look_up(my_spider)
+    #             # look_down(my_spider)
+    #             # warm_up(my_spider)
+    #             # push_up(my_spider)
 
-	except KeyboardInterrupt:
-		pass
-	except Exception as e:
-		print(f'Error:\n {e}')
-	finally:
-		crawler.do_action("stand", speed=60)
-		sleep(.1)		
-	 
+    # except KeyboardInterrupt:
+    #     pass
+    # except Exception as e:
+    #     print(f'Error:\n {e}')
+    # finally:
+    #     crawler.do_action("stand", speed=60)
+    #     sleep(.1)		
+    '''
  
 if __name__ == "__main__":
-	main()
+    main()
