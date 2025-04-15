05baba5973b5730e1f8dfaec8ffe7f05f34fb49a
Christian Salvador <41243724+KittenInAMitten@users.noreply.github.com>
2025-04-15 14:44:03 -0600

    New Code

--- scripts/app.py
+++ scripts/app.py
@@ -21,192 +21,6 @@ lastBlank = False
 
 # Debug variables
 KEYBOARD_MODE = True
-    
-
-manual = '''
-Press keys on keyboard to control PiCrawler!
-    W: Forward
-    A: Turn left
-    S: Backward
-    D: Turn right
-    R: Tilt up
-    F: Tilt down
-
-    Ctrl^C: Quit
-'''
-
-'''
-# def show_info():
-#     # print("\033[H\033[J",end='')  # clear terminal windows 
-#     print(manual)
-
-
-# def move_forward():
-#     global tilt_value
-#     global in_action
-#     if in_action:
-#         return
-#     in_action = True
-#     tilt_value = 0
-#     crawler.do_action('forward',1,speed)
-#     crawler.do_action('stand', speed)
-#     print("Moved forward")
-#     show_info()
-#     in_action = False
-#     return
-    
-# def move_backward():
-#     global tilt_value
-#     global in_action
-#     if in_action:
-#         return
-#     in_action = True
-#     tilt_value = 0
-#     crawler.do_action('backward',1,speed)
-#     crawler.do_action('stand', speed)
-#     print("Moved backward")
-#     show_info()
-#     in_action = False
-#     return
-    
-# def turn_left():
-#     global tilt_value
-#     global in_action
-#     if in_action:
-#         return
-#     in_action = True
-#     tilt_value = 0
-#     crawler.do_action('turn left',1,speed)
-#     crawler.do_action('stand', speed)
-#     print("Turned left")
-#     show_info()
-#     in_action = False
-#     return
-    
-# def turn_right():
-#     global tilt_value
-#     global in_action
-#     if in_action:
-#         return
-    
-#     in_action = True
-#     tilt_value = 0
-#     crawler.do_action('turn right',1,speed)
-#     crawler.do_action('stand', speed)
-#     print("Turned right")
-#     show_info()
-#     in_action = False
-#     return
-
-# def tilt_up():
-#     global tilt_value
-#     global in_action
-#     if in_action:
-#         return
-    
-#     in_action = True
-#     if tilt_value < 25:
-#         tilt_value += 5
-        
-#     coords = [
-#         # stand
-#         [[45, 45, (-50 - tilt_value)], [45, 45, (-50 - tilt_value)], [45, 45, (-50 + tilt_value)], [45, 45, (-50 + tilt_value)]]
-#     ]
-#     for coord in coords:
-#         crawler.do_step(coord, 60)
-#     show_info()
-#     in_action = False
-#     return
-#     # crawler.do_action('look up',1,speed)
-#     # print("Tilted up")
-#     # show_info()
-#     # return
-
-# def tilt_down():
-#     global tilt_value
-#     global in_action
-#     if in_action:
-#         return
-#     in_action = True
-    
-#     if tilt_value > -25:
-#         tilt_value -= 5
-#     coords = [
-#         # stand
-#         [[45, 45, (-50 - tilt_value)], [45, 45, (-50 - tilt_value)], [45, 45, (-50 + tilt_value)], [45, 45, (-50 + tilt_value)]]
-#     ]
-#     for coord in coords:
-#         crawler.do_step(coord, 60)
-#     in_action = False
-#     return
-
-#     # crawler.do_action('look down',1,speed)
-#     # print("Tilted down")
-#     # return
- 
-# def stand():
-#     global tilt_value
-#     global in_action
-#     if in_action:
-#         return
-#     in_action = True
-#     crawler.do_action("stand", speed=60)
-#     # coords = [
-#     #     # stand
-#     #     [[45, 45, -50], [45, 45, -50], [45, 45, -50], [45, 45, -50]],
-#     #     [[45, 45, -75], [45, 45, -75], [45, 45, -35], [45, 45, -35]],
-#     # ]
-#     # for coord in coords:
-#     #     crawler.do_step(coord, 60)
-#     in_action = False
-#     return
-
-def lock_pos():
-    # global in_action
-    # if in_action:
-    #     return
-    # print('test')
-    # # coords = crawler.current_step_all_leg_value()
-    # # print(coords)
-    # for coord in [coords]:
-    #     # crawler.do_step(coord, 60)
-    #     # print(coord)
-    return
-
-def main_loop(): 
-
-    # Vilib.camera_start()
-    # Vilib.display()
-    # Vilib.color_detect("red")
-    print("is happening")
-    show_info()   
-    if(KEYBOARD_MODE):
-        while True:
-            key = readchar.readkey()
-            key = key.lower()
-            if key in('wsad'):
-                if 'w' == key:
-                    move_forward()   
-                elif 's' == key:
-                    move_backward()         
-                elif 'a' == key:
-                    turn_left()          
-                elif 'd' == key:
-                    turn_right()
-                elif 'r' == key:
-                    tilt_up()
-                elif 'f' == key:
-                    tilt_down()
-                sleep(0.05)
-                show_info()  
-
-            elif key == readchar.key.CTRL_C:
-                print("\n Quit") 
-                break    
-            
-            sleep(0.02)          
-
-'''
 
 app = Flask(__name__)
 
@@ -228,33 +42,38 @@ def command():
     # if data['action'] != "lock": 
     if waiting == False:
         start_time = time.time()
-        print(f"Received command: {data['action']}")
+        if data['action'] != 'blank':
+            print(f"Received command: {data['action']}")
         waiting = True
         lastBlank = False
-        if data['action'] == "forward":
-            clientsocket.send(bytes(data['action'], 'UTF-8'))
-            # move_forward()
-        elif data['action'] == "backward":
-            clientsocket.send(bytes(data['action'], 'UTF-8'))
-            # move_backward()
-        elif data['action'] == "left":
-            clientsocket.send(bytes(data['action'], 'UTF-8'))
-            # turn_left()
-        elif data['action'] == "right":
-            clientsocket.send(bytes(data['action'], 'UTF-8'))
-            # turn_right()
-        elif data['action'] == "tilt_up":
-            clientsocket.send(bytes(data['action'], 'UTF-8'))
-            # tilt_up()
-        elif data['action'] == "tilt_down":
-            clientsocket.send(bytes(data['action'], 'UTF-8'))
-        elif data['action'] == "setAutomatic":
-            clientsocket.send(bytes(data['action'], 'UTF-8'))
-        elif data['action'] == "setManual":
-            clientsocket.send(bytes(data['action'], 'UTF-8'))
-        elif data['action'] == "blank":
+        clientsocket.send(bytes(data['action'], 'UTF-8'))
+        
+        # if data['action'] == "forward":
+        #     clientsocket.send(bytes(data['action'], 'UTF-8'))
+        #     # move_forward()
+        # elif data['action'] == "backward":
+        #     clientsocket.send(bytes(data['action'], 'UTF-8'))
+        #     # move_backward()
+        # elif data['action'] == "left":
+        #     clientsocket.send(bytes(data['action'], 'UTF-8'))
+        #     # turn_left()
+        # elif data['action'] == "right":
+        #     clientsocket.send(bytes(data['action'], 'UTF-8'))
+        #     # turn_right()
+        # elif data['action'] == "tilt_up":
+        #     clientsocket.send(bytes(data['action'], 'UTF-8'))
+        #     # tilt_up()
+        # elif data['action'] == "tilt_down":
+        #     clientsocket.send(bytes(data['action'], 'UTF-8'))
+        # elif data['action'] == "setAutomatic":
+        #     clientsocket.send(bytes(data['action'], 'UTF-8'))
+        # elif data['action'] == "setManual":
+        #     clientsocket.send(bytes(data['action'], 'UTF-8'))
+        # elif data['action'] == "take_picture":
+        #     clientsocket.send(bytes(data['action'], 'UTF-8'))
+        # elif data['action'] == "blank":
+        #     clientsocket.send(bytes(data['action'], 'UTF-8'))
             # lastBlank = True
-            clientsocket.send(bytes(data['action'], 'UTF-8'))
             # tilt_down()
         # elif data['action'] == "lock":
         #     lock_pos()
@@ -264,6 +83,7 @@ def command():
             buf = clientsocket.recv(64)
             reply = buf.decode('utf-8')
             if len(buf) > 0:
+                print(reply)
                 if reply == 'ok':
                     waiting = False
                     break
@@ -280,6 +100,7 @@ def video_feed():
     print("video started")
     # return Vilib.display()
     return "http://172.17.10.168:9000/mjpg"
+
 
 if __name__ == '__main__':
     # control_thread = threading.Thread(target=main_loop, daemon=True)
--- scripts/main.py
+++ scripts/main.py
@@ -2,6 +2,11 @@ from vilib import Vilib
 from picrawler import Picrawler
 from time import sleep
 import socket
+import os
+import time
+
+import cv2
+import numpy as np
 
 # Variables
 crawler = Picrawler() 
@@ -10,6 +15,10 @@ tilt_value = 0
 in_action = False
 
 student_colors = ["green", "yellow", "blue"]
+
+detected = [0,0,0]
+
+last_time = 0
 
 ALERT_MODE = False
 MANUAL_MODE = True
@@ -135,20 +144,24 @@ def tilt_down():
     # print("Tilted down")
     # return
  
-def stand():
+def doNothing():
     global tilt_value
     global in_action
     if in_action:
         return
     in_action = True
-    if tilt_value > 0:
-        tilt_up()
-    elif tilt_value < 0:
-        tilt_down()
-    else: 
-        crawler.do_action("stand", speed=60)
-        sleep(0.05)    
-        in_action = False
+    coords = [
+        # stand
+        [[45, 45, (-50 - tilt_value)], [45, 45, (-50 - tilt_value)], [45, 45, (-50 + tilt_value)], [45, 45, (-50 + tilt_value)]]
+    ]
+    for coord in coords:
+        crawler.do_step(coord, 60)
+    sleep(0.05)
+    in_action = False
+    # else: 
+    #     crawler.do_action("stand", speed=60)
+    #     sleep(0.05)    
+    #     in_action = False
     # coords = [
     #     # stand
     #     [[45, 45, -50], [45, 45, -50], [45, 45, -50], [45, 45, -50]],
@@ -184,6 +197,119 @@ def followRed():
         sleep(0.05)
         in_action = False
         
+def takePicture():# ----- check path -----
+    global last_time
+    global in_action
+    
+    #check if time elapsed is 10 seconds, return if not
+    if time.time() - last_time < 20 or in_action:
+        return
+    
+    in_action = True
+    #re-update time
+    last_time = time.time()
+    
+    user_name = os.getlogin()
+    path = f"/home/motherflocker/picrawler/custom/scripts/static/images"
+    if not os.path.exists(path):
+        # print('Path does not exist. Creating path now ... ')
+        os.makedirs(name=path, mode=0o751, exist_ok=True)
+        time.sleep(0.01) 
+
+    # if status:
+    #     print('The photo is saved as '+path+'/'+photo_name+'.jpg')
+    # else:
+    #     print('Photo save failed .. ')
+
+    current_img = Vilib.img
+    analyzePicture(current_img)
+    
+    count = 0
+    for x in range(3): 
+        if detected[x] == 1:
+            # ----- save photo -----
+            status = False
+            for _ in range(5):
+                if  current_img is not None:
+                    status = cv2.imwrite(path + '/' + student_colors[x] +'.jpg', current_img)
+                    break
+                else:
+                    time.sleep(0.01)
+            else:
+                status = False
+            if status:
+                print(path + '/' + student_colors[x] + '.jpg updated.')
+            else:
+                print(path + '/' + student_colors[x] + '.jpg not updated.')
+        count = count + 1
+    
+    sleep(0.05)
+    in_action = False
+    return 
+
+def analyzePicture(img):
+    global detected
+    color_dict = {
+        'red':[[0, 8], [80, 255], [0, 255]],
+        'orange':[[12, 18], [80, 255], [80, 255]],
+        'yellow':[[20, 60], [60, 255], [120, 255]],
+        'green':[[45, 85], [120, 255], [80, 255]],
+        'blue':[[92,120], [120, 255], [80, 255]],
+        'purple':[[115,155], [30, 255], [60, 255]],
+        'magenta':[[160,180], [30, 255], [60, 255]],
+    }
+    
+    for x in range(3):
+        '''Define parameters for color detection object'''
+        
+        color_name = student_colors[x]
+        
+        # color_obj_parameter['color'] = color_name   
+        
+        # Reduce image for faster recognition 
+        zoom = 4 # reduction ratio
+        width_zoom = int(Vilib.camera_width / zoom)
+        height_zoom = int(Vilib.camera_height / zoom)
+        resize_img = cv2.resize(img, (width_zoom, height_zoom), interpolation=cv2.INTER_LINEAR)
+        
+        # Convert the image in BGR to HSV
+        hsv = cv2.cvtColor(resize_img, cv2.COLOR_BGR2HSV) 
+    
+        # Set range for red color and define mask
+        # color_lower = np.array([min(color_dict[color_name]), 60, 60])
+        # color_upper = np.array([max(color_dict[color_name]), 255, 255])
+        color_lower = np.array([min(color_dict[color_name][0]), min(color_dict[color_name][1]), min(color_dict[color_name][2])])
+        color_upper = np.array([max(color_dict[color_name][0]), max(color_dict[color_name][1]), max(color_dict[color_name][2])])
+        
+        mask = cv2.inRange(hsv, color_lower, color_upper)
+
+        if color_name == 'red':
+            mask_2 = cv2.inRange(hsv, (167, 0, 0), (180, 255, 255))
+            mask = cv2.bitwise_or(mask, mask_2)
+
+        # define a 5*5 kernel
+        kernel_5 = np.ones((5,5), np.uint8)
+
+        # opening the image (erosion followed by dilation), to remove the image noise
+        open_img = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel_5, iterations=1)      
+
+        # Find contours in binary image
+        _tuple = cv2.findContours(open_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+        # compatible with opencv3.x and openc4.x
+        if len(_tuple) == 3:
+            _, contours, hierarchy = _tuple
+        else:
+            contours, hierarchy = _tuple
+        
+        if(len(contours) > 0):
+            detected[x] = 1
+        else:
+            detected[x] = 0
+        
+
+    
+    return
+        
 # def checkForColors():
 #     detection = [0, 0, 0]
 #     for colorIndex in range(3):
@@ -207,6 +333,7 @@ def main():
     Vilib.color_detect("red") 
     
     global MANUAL_MODE
+    global in_action
     
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serversocket.bind(('localhost', 8089))
@@ -221,6 +348,7 @@ def main():
             command = buf.decode('utf-8')
             if len(buf) > 0:
                 print('Received command:' + command)
+                takePicture()
                 if MANUAL_MODE:
                     if command == "forward":
                         move_forward()	
@@ -237,7 +365,9 @@ def main():
                     elif command == "setAutomatic":
                         MANUAL_MODE = False
                     elif command == "blank":
-                        stand()
+                        doNothing()
+                    # elif command == "take_picture":
+                    #     takePicture()
                 else:
                     if command == "setManual":
                         MANUAL_MODE = True
@@ -246,6 +376,7 @@ def main():
                         print("update")
                         followRed()
                 connection.send(bytes('ok', 'UTF-8'))
+                print(*detected, end="\t")
                 sleep(0.05)
     except KeyboardInterrupt:
         pass
